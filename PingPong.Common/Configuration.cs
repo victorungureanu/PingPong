7ee94aa19005a26de064b031c7d6023973b44c35
Victor Ungureanu <victor.ungureanu@reedbusiness.com>
2018-10-16 15:56:12 +0100

    call unmanaged code to transform and filter image

---
+++ PingPong.Common/Configuration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPong.Common
+{
+    public class Configuration
+    {
+        public virtual int FrameWidth => 640;
+        public virtual int FrameHeight => 640;
+
+        public virtual double HueMin => 0;
+        public virtual double HueMax => 32;
+
+        public virtual double SaturationMin => 139;
+        public virtual double SaturationMax => 212;
+
+        public virtual double ValueMin => 182;
+        public virtual double ValueMax => 256;
+
+        public virtual double Erode => 3;
+        public virtual double Dilate => 8;
+
+        public virtual bool ShowDebugWindows => true;
+    }
+}
--- PingPong.Tracking/VideoProcessor.cs
+++ PingPong.Tracking/VideoProcessor.cs
@@ -1,21 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Emgu.CV;
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
 using log4net;
+using PingPong.Common;
 
 namespace PingPong.Tracking
 {
     public class VideoProcessor
     {
-        private ILog log;
+        private readonly ILog log;
+        private readonly Configuration configuration;
 
-        public VideoProcessor(ILog log)
+        private readonly Mat cameraFeed = new Mat();
+        private readonly Mat hsv = new Mat();
+        private readonly Mat threshold = new Mat();
+
+        public VideoProcessor(ILog log, Configuration configuration)
         {
             this.log = log;
+            this.configuration = configuration;
         }
+
+        public Mat CameraFeed => cameraFeed;
+        public Mat HSV => hsv;
+        public Mat Thresold => threshold;
+
+
+        public bool ShouldStop { get; set; } = false;
 
         public void Process(string filePath)
         {
@@ -28,8 +39,41 @@ namespace PingPong.Tracking
                 return;
             }
 
-            ;
-        }
+            //TODO: why am I doing this? what happens if I don't?
+            capture.SetCaptureProperty(CapProp.FrameWidth, configuration.FrameWidth);
+            capture.SetCaptureProperty(CapProp.FrameHeight, configuration.FrameHeight);
 
+
+            var frameNo = 0;
+
+            while (!ShouldStop)//TODO: figure out when to stop
+            {
+                capture.Read(cameraFeed);
+
+                CvInvoke.CvtColor(cameraFeed, hsv, ColorConversion.Bgr2Hsv);
+
+                var minArray = new ScalarArray(new MCvScalar(
+                    configuration.HueMin,
+                    configuration.SaturationMin,
+                    configuration.ValueMin));
+
+                var maxArray = new ScalarArray(new MCvScalar(
+                    configuration.HueMax,
+                    configuration.SaturationMax,
+                    configuration.ValueMax));
+
+                CvInvoke.InRange(hsv, minArray, maxArray, threshold);
+
+                if (configuration.ShowDebugWindows)
+                {
+                    CvInvoke.Imshow("Original Image", cameraFeed);
+                    CvInvoke.Imshow("HSV Image", hsv);
+                    CvInvoke.Imshow("Threshold", threshold);
+
+                    CvInvoke.WaitKey(30);
+                }
+            }
+
+        }
     }
 }
